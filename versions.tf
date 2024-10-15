419fd94f39c972f55e136c0faf109704045bc60b
briskt <3172830+briskt@users.noreply.github.com>
2024-10-15 09:36:09 +0800

    edit comment for readability

--- versions.tf
+++ versions.tf
@@ -9,8 +9,8 @@ terraform {
     cloudflare = {
       source = "cloudflare/cloudflare"
 
-      // 4.39.0 and later versions deprecated cloudflare_record.value
-      // constraining to earlier versions while waiting for version 5 to mature
+      // 4.39.0 deprecated cloudflare_record.value
+      // While waiting for version 5 to mature, we'll constrain to earlier versions.
       version = ">= 2.0.0, < 4.39.0"
     }
   }
