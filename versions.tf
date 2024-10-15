5ff81d968dadf63e38452b03fcb804714b0f0538
briskt <3172830+briskt@users.noreply.github.com>
2024-10-15 09:34:30 +0800

    update Cloudflare provider to v4

--- versions.tf
+++ versions.tf
@@ -7,8 +7,11 @@ terraform {
       version = ">= 2.0.0, < 6.0.0"
     }
     cloudflare = {
-      source  = "cloudflare/cloudflare"
-      version = ">= 2.0.0, < 4.0.0"
+      source = "cloudflare/cloudflare"
+
+      // 4.39.0 and later versions deprecated cloudflare_record.value
+      // constraining to earlier versions while waiting for version 5 to mature
+      version = ">= 2.0.0, < 4.39.0"
     }
   }
 }
